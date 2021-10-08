722dc7944c995a42e5c6013d3ed05df3fcf8956f
Jordan Elver <jordan@elver.me>
2021-10-08 22:59:01 +0100

    Always annotate the latest commit on the branch

--- lib/index.rb
+++ lib/index.rb
@@ -19,9 +19,18 @@ def files_to_check?
   !ENV["FILES_TO_CHECK"].empty?
 end
 
+def commit_to_annotate(event)
+  # The value of GITHUB_SHA differs based on the event type.
+  # For pull_request events GITHUB_SHA is "Last merge commit on the GITHUB_REF branch"
+  # For push events GITHUB_SHA is "Commit pushed, unless deleting a branch"
+  # As such we use the `after` attribute which should always point to the
+  # latest commit on the branch.
+  event["after"]
+end
+
 @event_json = read_json(ENV["GITHUB_EVENT_PATH"]) if ENV["GITHUB_EVENT_PATH"]
 @github_data = {
-  sha: ENV["GITHUB_SHA"],
+  sha: commit_to_annotate(@event_json),
   token: ENV["GITHUB_TOKEN"],
   owner: ENV["GITHUB_REPOSITORY_OWNER"] || @event_json.dig("repository", "owner", "login"),
   repo: ENV["GITHUB_REPOSITORY_NAME"] || @event_json.dig("repository", "name"),
